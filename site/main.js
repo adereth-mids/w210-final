90d9b593c0373b4bf94f7b9fddb6caa66bbfbac4
Aithne Feay <aidanlfeay@gmail.com>
2019-12-08 14:13:34 -0500

    Add D3 Versions of Modeling Charts

--- site/main.js
+++ site/main.js
@@ -1,3 +1,99 @@
+const loadPartyTermHistogram = (data) => {
+    const margin = {top: 20, right: 20, bottom: 70, left: 40},
+        width = 600 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+    const blueHex = "#495D70";
+    const redHex = "#CC6868";
+
+    const x0 = d3.scale.ordinal().rangeRoundBands([20, width], .1);
+
+    const x1 = d3.scale.ordinal();
+
+    const y = d3.scale.linear().range([height, 0]);
+
+    const xAxis = d3.svg.axis().scale(x0).tickSize(0).orient("bottom");
+
+    const yAxis = d3.svg.axis().scale(y).orient("left").tickFormat(function(t) { return `${t}k`});
+
+    const color = d3.scale.ordinal().range([redHex, blueHex]);
+
+    d3.select("#party-term-histogram").html("");
+    const svg = d3.select('#party-term-histogram').append("svg").attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    const categoriesNames = data.map(function(d) { return d.category; });
+    const parties = data[0].values.map(function(d) { return d.party; });
+
+    x0.domain(categoriesNames);
+    x1.domain(parties).rangeRoundBands([0, x0.rangeBand()]);
+    y.domain([0, 60]);
+
+    svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis);
+
+    svg.append("g").attr("class", "y axis").call(yAxis).append("text").attr("transform", "rotate(-90)")
+       .attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text("No. Statements");
+
+    const slice = svg.selectAll(".slice").data(data).enter().append("g").attr("class", "g")
+                   .attr("transform",function(d) { return "translate(" + x0(d.category) + ",0)"; });
+
+    slice.selectAll("rect").data(function(d) { return d.values; }).enter().append("rect").attr("width", x1.rangeBand())
+         .attr("x", function(d) { return x1(d.party); }).style("fill", function(d) { return color(d.party) })
+         .attr("y", function(d) { return y(0); }).attr("height", function(d) { return height - y(0); })
+
+    slice.selectAll("rect").attr("y", function(d) { return y(d.statements); }).attr("height", function(d) { return height - y(d.statements); });
+
+    slice.selectAll(".text").data(function(d) { return d.values; }).enter().append("text").attr("class","label-small").attr("x", (function(d) { return x1(d.party) + 8 }  ))
+         .attr("y", function(d) { return y(d.statements) - 15; }).attr("dy", ".75em").text(function(d) { return `${d.statements}k`; });
+}
+
+const loadModelHistogram = (data) => {
+    const margin = {top: 20, right: 20, bottom: 70, left: 40},
+        width = 600 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+
+    const blueHex = "#495D70";
+
+    const x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+    const y = d3.scale.linear().range([height, 0]);
+
+    const xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+
+    const yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(3);
+
+    d3.select("#model-performance-histogram").html("");
+
+    const svg = d3.select("#model-performance-histogram").append("svg").attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom).append("g")
+                    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+
+    data.forEach(function(d) {
+        d.model = d.model;
+        d.accuracy = +d.accuracy;
+    });
+
+    x.domain(data.map(function(d) { return d.model; }));
+    y.domain([.25, d3.max(data, function(d) { return d.accuracy + 0.10; })]);
+
+    svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")")
+       .call(xAxis).selectAll("text").style("text-anchor", "middle")
+
+    svg.append("g").attr("class", "y axis").call(yAxis).append("text").attr("transform", "rotate(-90)").attr("y", 6)
+       .attr("dy", ".71em").style("text-anchor", "end").text("Accuracy");
+
+    svg.selectAll("bar").data(data).enter().append("rect").style("fill", blueHex).attr("x", function(d) { return x(d.model); })
+       .attr("width", x.rangeBand()).attr("y", function(d) { return y(d.accuracy); }).attr("height", function(d) { return height - y(d.accuracy); });
+
+    svg.selectAll(".text").data(data).enter().append("text").attr("class","label").attr("x", (function(d) { return x(d.model) + 60 }  ))
+       .attr("y", function(d) { return y(d.accuracy) - 25; }).attr("dy", ".75em").text(function(d) { return `${d.accuracy}%`; });
+}
+
 const loadLineChart = (data) => {
 
     const margin = {top: 30, right: 20, bottom: 70, left: 110},
@@ -34,7 +130,7 @@ const loadLineChart = (data) => {
         d.value = (d.predicted_party === 'republican' ? d.value : 50 - (d.value - 50));
     });
 
-    var valueline = d3.svg.line().x(function(d) { return x(d.date); }).y(function(d) { return y(d.value); });
+    const valueline = d3.svg.line().x(function(d) { return x(d.date); }).y(function(d) { return y(d.value); });
 
     d3.select("#graphic").html("");
 
@@ -139,31 +235,95 @@ const loadData = (keyword, limit, callback) => {
 
 const reload = () => {
     const keyword = document.getElementById('keyword').value;
-    loadData(keyword, '1000', (data) => {
-        // loadBarChart(data);
+    loadData(keyword, '100', (data) => {
         loadLineChart(data);
     });
 }
 
-// window.currentSection = 0;
-// const scrollToNextSection = () => {
-//     $('html, body').animate({
-//         scrollTop: $(`#waypoint-${window.currentSection + 1}`).offset().top,
-//         easing: 'easeout',
-//     }, 1000);
-//     window.currentSection += 1;
-// }
-
 $(document).ready(function() {
-    // const waypoints = [];
-    // for (let i = 0; i < 7; i++) {
-    //     const waypoint = new Waypoint({
-    //         element: document.getElementById(`waypoint-${i}`),
-    //         handler: function() { window.currentSection = i }
-    //     });
-    //     waypoints.push(waypoint);
-    // }
-
     const rellax = new Rellax('.rellax', { center: true });
+    const partyTermData = [
+        {
+            category: "'95-01",
+            values: [
+                {
+                    party: "republican",
+                    statements: 59.3,
+                },
+                {
+                    party: "democrat",
+                    statements: 39.5,
+                }
+            ]
+        },
+        {
+            category: "'01-05",
+            values: [
+                {
+                    party: "republican",
+                    statements: 27.7,
+                },
+                {
+                    party: "democrat",
+                    statements: 31.1,
+                }
+            ]
+        },
+        {
+            category: "'05-09",
+            values: [
+                {
+                    party: "republican",
+                    statements: 24.4,
+                },
+                {
+                    party: "democrat",
+                    statements: 24.7,
+                }
+            ]
+        },
+        {
+            category: "'09-13",
+            values: [
+                {
+                    party: "republican",
+                    statements: 11.8,
+                },
+                {
+                    party: "democrat",
+                    statements: 23.9,
+                }
+            ]
+        },
+        {
+            category: "'13-17",
+            values: [
+                {
+                    party: "republican",
+                    statements: 13.1,
+                },
+                {
+                    party: "democrat",
+                    statements: 12.8,
+                }
+            ]
+        },
+    ];
+    loadPartyTermHistogram(partyTermData);
+    const modelData = [
+        {
+            model: 'Random Forest',
+            accuracy: 0.4390,
+        },
+        {
+            model: 'CNN',
+            accuracy: 0.6428,
+        },
+        {
+            model: 'RNN',
+            accuracy: 0.6508,
+        },
+    ];
+    loadModelHistogram(modelData);
     reload('fiscal');
 });
